8fca1006e98ffef41201c88420d5e5f1aee76138
outlier007 <1504126196@qq.com>
2021-10-12 14:41:28 +0800

    计算闰年

---
+++ leap_year/leap_year/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int is_leap_year(int y)//¼ÆËãÈòÄê
+{
+	if ((y % 4 == 0 && y % 100 != 0)||(y%400 == 0))
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int year = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (1 == is_leap_year(year))
+			printf("%d ", year);
+	}
+	return 0;
+}
